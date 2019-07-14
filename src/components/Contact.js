661422b6147e6a13bf8a4b5849bf4b47cd7440f8
mike <mikecee0815@gmail.com>
2019-07-14 12:12:32 -0400

    contact v1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,34 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import DarthVader from '../Darth-Vader-icon.png'
 
 const Contact = () => {
     let contactStyle = {
         background:'orange',
-        padding:20,
+        padding:30,
         boxSizing:'borderBox',
-        width:'95%',
-        height:400,
+        width:'85%',
         margin:'auto',
         boxShadow: '4px 5px 10px',
-        marginTop:20
+        marginTop:20,
+        marginBottom:20,
+        borderRadius:7,
+        imgSmall:{
+            width:175,
+            height:175
+        }
     }
     return(
         <div style={contactStyle}>
-            <h2 className="display-3">Contact</h2>
+            <h2 className="display-4">Contact</h2>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. See my <Link to={'/about'}>Portfolio</Link> page for more information
             </p>
+            <img 
+                src={DarthVader}
+                style={contactStyle.imgSmall} 
+                className="rounded-circle mx-auto d-block" alt="DarthVader png"
+                />
         </div>
     )
 }
